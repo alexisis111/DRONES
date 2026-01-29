ccb291194977407e6190e7b4ad03c8c8fec0ddff
Александр Гилев <shuragilyow@yandex.ru>
2026-01-29 13:14:13 +0300

    ntl

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,20 @@ import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),netlifyReactRouter()],
+  plugins: [react(), tailwindcss(), netlifyReactRouter()],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'react-vendor';
+          }
+          if (id.includes('node_modules/@react-three') || id.includes('node_modules/three')) {
+            return 'three-vendor';
+          }
+        }
+      }
+    }
+  }
 })
